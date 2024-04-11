0cd4f297c9f67962b7b6c7bf4439fac3110d6303
SamDesktop <svelasco514@gmail.com>
2024-04-11 05:15:06 -0400

    added hop/nvim
    <leader>fj

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -7,6 +7,24 @@ return {
     end,
   },
 
+  {
+    "smoka7/hop.nvim",
+    opts = {
+      multi_windows = true,
+      keys = "htnsueoaidgcrlypmbkjvx",
+      uppercase_labels = true,
+    },
+    keys = {
+      {
+        "<leader>fj",
+        function()
+          require("hop").hint_words()
+        end,
+        mode = { "n", "x", "o" },
+      },
+    },
+  },
+
   -- These are some examples, uncomment them if you want to see them work!
   -- {
   --   "neovim/nvim-lspconfig",
