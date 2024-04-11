8c9629510fe69796627adc2367be78273a7f876a
SamDesktop <svelasco514@gmail.com>
2024-04-11 18:40:57 -0400

    made copilot work

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,25 @@
 require "nvchad.mappings"
 
--- add yours here
+-- Add your mappings here
 
 local map = vim.keymap.set
 
+-- Example key mappings
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- Initialize M if it's not already initialized
+M = M or {}
+
+-- Add Copilot mappings
+M.copilot = {
+  i = {
+    ["<C-l>"] = {
+      function()
+        vim.fn.feedkeys(vim.fn["copilot#Accept"](), "")
+      end,
+      "Copilot Accept",
+      { replace_keycodes = true, nowait = true, silent = true, expr = true, noremap = true },
+    },
+  },
+}
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -24,34 +24,16 @@ return {
       },
     },
   },
-  -- https://www.reddit.com/r/neovim/comments/12vcybk/how_to_get_copilot_to_work_on_nvchad/
-  -- Making copilot work somehow
   {
-    "zbirenbaum/copilot.lua",
-    event = "InsertEnter",
-    opts = {},
-  },
-
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      {
-        "zbirenbaum/copilot-cmp",
-        config = function()
-          require("copilot_cmp").setup()
-        end,
-      },
-    },
-    opts = {
-      sources = {
-        { name = "nvim_lsp", group_index = 2 },
-        { name = "copilot", group_index = 2 },
-        { name = "luasnip", group_index = 2 },
-        { name = "buffer", group_index = 2 },
-        { name = "nvim_lua", group_index = 2 },
-        { name = "path", group_index = 2 },
-      },
-    },
+    "github/copilot.vim",
+    lazy = false,
+    config = function() -- Mapping tab is already used by NvChad
+      vim.g.copilot_no_tab_map = true
+      vim.g.copilot_assume_mapped = true
+      vim.g.copilot_tab_fallback = ""
+      -- The mapping is set to other key, see custom/lua/mappings
+      -- or run <leader>ch to see copilot mapping section
+    end,
   },
 
   -- These are some examples, uncomment them if you want to see them work!
