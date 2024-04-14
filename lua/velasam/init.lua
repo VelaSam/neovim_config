491dfa89506d67b222134e47d76152fe7dfa939b
VelaSam <svelasco514@gmail.com>
2024-04-13 22:44:01 -0400

    copy paste from clipboard

--- lua/velasam/init.lua
+++ lua/velasam/init.lua
@@ -1,2 +1,1 @@
-print("hello from samvel")
 require("remap")
--- lua/velasam/remap.lua
+++ lua/velasam/remap.lua
@@ -2,7 +2,14 @@ print("hello world from remap")
 vim.g.mapleader = " "
 vim.keymap.set({"n", "i", "v"}, "<C-s>", "<ESC>:w<CR>")
 
-function hello()
-	print("hello world")
-end
+-- Copy to clipboard
+vim.api.nvim_set_keymap('v', '<leader>y', '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>Y', '"+yg_', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>y', '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>yy', '"+yy', { noremap = true, silent = true })
 
+-- Paste from clipboard
+vim.api.nvim_set_keymap('n', '<leader>p', '"+p', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>P', '"+P', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<leader>p', '"+p', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<leader>P', '"+P', { noremap = true, silent = true })
