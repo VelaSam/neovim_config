9b9729704c895a197847134c29668e3e233ba3d5
Samuel Velasco <93353011+VelaSam@users.noreply.github.com>
2024-04-24 20:40:29 -0400

    devicons

--- lua/velasam/plugins/init.lua
+++ lua/velasam/plugins/init.lua
@@ -1,15 +1,14 @@
-return 
-{
-	{"nvim-tree/nvim-tree.lua"},
-{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-
-{
-    "williamboman/mason.nvim"
-},
-	{
-		"folke/which-key.nvim",
-	},
-        {
+return {
+    {"nvim-tree/nvim-tree.lua"},
+    {"nvim-tree/nvim-web-devicons"},
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    {
+        "williamboman/mason.nvim"
+    },
+    {
+        "folke/which-key.nvim",
+    },
+    {
         "smoka7/hop.nvim",
         opts = {
             multi_windows = true,
@@ -26,6 +25,4 @@ return
             },
         },
     },
-
-
 }
