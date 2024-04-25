9364c4dd32c5107f1343f128ab8d18fffa675ab4
Samuel Velasco <93353011+VelaSam@users.noreply.github.com>
2024-04-24 20:42:19 -0400

    change for vim keymap set

--- lua/velasam/remap.lua
+++ lua/velasam/remap.lua
@@ -2,16 +2,17 @@
 vim.keymap.set({"n", "i", "v"}, "<C-s>", "<ESC>:w<CR>")
 
 -- Copy to clipboard
-vim.api.nvim_set_keymap('v', 'y', '"+y', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'Y', '"+yg_', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'y', '"+y', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'yy', '"+yy', { noremap = true, silent = true })
+vim.keymap.set('v', 'y', '"+y', { noremap = true, silent = true })
+vim.keymap.set('n', 'Y', '"+yg_', { noremap = true, silent = true })
+vim.keymap.set('n', 'y', '"+y', { noremap = true, silent = true })
+vim.keymap.set('n', 'yy', '"+yy', { noremap = true, silent = true })
 
 -- Paste from clipboard
-vim.api.nvim_set_keymap('n', 'p', '"+p', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'P', '"+P', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', 'p', '"+p', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', 'P', '"+P', { noremap = true, silent = true })
+vim.keymap.set('n', 'p', '"+p', { noremap = true, silent = true })
+vim.keymap.set('n', 'P', '"+P', { noremap = true, silent = true })
+vim.keymap.set('v', 'p', '"+p', { noremap = true, silent = true })
+vim.keymap.set('v', 'P', '"+P', { noremap = true, silent = true })
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
