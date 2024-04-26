1c3acdfdd52bf82727d7d48a4cf7a707df23fb92
Samuel Velasco <93353011+VelaSam@users.noreply.github.com>
2024-04-26 10:07:37 -0400

    more opts

--- lua/velasam/options.lua
+++ lua/velasam/options.lua
@@ -14,15 +14,39 @@ vim.opt.smartcase = true
 
 vim.opt.breakindent = true
 
-vim.wo.relativenumber = true
+vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.cursorline = true
+vim.opt.undofile = true
+
+vim.opt.signcolumn = 'yes'
+
+vim.opt.updatetime = 250
+
+-- time for you to do the keybinds in miliseconds
+-- vim.opt.timeoutlen = 300
+
+-- split right: <C-w> and then v 
+vim.opt.splitright = true
+--split beloe <C-w> and then s
+vim.opt.splitbelow = true
+
+vim.opt.list = true
+vim.opt.listchars = {tab = '» ', trail = '.', nbsp = '␣' }
+
+vim.opt.scrolloff = 8
+
+vim.opt.hlsearch = true
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+vim.opt.termguicolors = true
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,
