bcd85ee0bb4742d139c607ed63076fd07bf1b2e6
Samuel Velasco <93353011+VelaSam@users.noreply.github.com>
2024-04-27 23:50:13 -0400

    test files

---
+++ test/randomfile.js
@@ -0,0 +1,31 @@
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function greet(name) {
+  console.log(`Hello, ${name}!`);
+}
+
+function main() {
+  rl.question('Welcome to the greeting program!\nEnter your name: ', (name) => {
+    greet(name);
+    
+    rl.question('Would you like to be greeted again? (yes/no) ', (response) => {
+      if (response.toLowerCase() === 'yes') {
+        rl.question('Enter another name: ', (anotherName) => {
+          greet(anotherName);
+          rl.close();
+        });
+      } else {
+        console.log('Goodbye!');
+        rl.close();
+      }
+    });
+  });
+}
+
+main();
