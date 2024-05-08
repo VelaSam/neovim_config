1b5c868890f200416ad78031de1c75995b34b7fc
Sam Velasco <svelasco514@gmail.com>
2024-05-08 16:50:42 -0400

    modified telescope for tab instead of ctrlhjkl

--- lua/velasam/configs/telescope.lua
+++ lua/velasam/configs/telescope.lua
@@ -14,9 +14,9 @@ return {
             defaults =  {
                 mappings = {
                     i = {
-                        ['<C-k>'] = actions.move_selection_previous,
-                        ['<C-j>'] = actions.move_selection_next,
-                        ['<C-q>'] = actions.send_selected_to_qflist + actions.open_qflist,
+                        ['<Tab>'] = actions.move_selection_previous,
+                        ['<S-Tab>'] = actions.move_selection_next,
+                        -- ['<C-q>'] = actions.send_selected_to_qflist + actions.open_qflist, what does this even do wtf
 
                     }
                 }
